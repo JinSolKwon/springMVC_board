8e99a564d386fde26cadd8e5f6fd564b7016e2d5
JinSolKwon <88972646+JinSolKwon@users.noreply.github.com>
2021-09-18 15:34:04 +0900

    Delete boardProject/src/main/java/action directory

--- boardProject/src/main/java/action/CommandAction.java
+++
@@ -1,8 +0,0 @@
-package action;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface CommandAction {
-	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable;
-}
